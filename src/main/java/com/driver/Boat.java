f866de728c677dc883ffe37181ccd019200f5b56
Abdul Rehman <mabdul6492@gmail.com>
2023-10-18 19:37:55 +0530

    implement both sub task

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,14 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    String name;
+    int capacity;
+
+    public String getVehicleName() {
+        return this.name;
+    }
+    public int getVehicleCapacity() {
+        return this.capacity;
+    }
+}
--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,11 +4,12 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 4, 6, isManual, "F1", 1);
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
+        int newSpeed = this.getCurrentSpeed()+rate; //set the value of new speed by using currentSpeed and rate
+        /*
          * speed 0: gear 1
          * speed 1-50: gear 1
          * speed 51-100: gear 2
@@ -18,13 +19,14 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
+        if(newSpeed > 250) this.changeGear(6);
+        else if(newSpeed > 200) this.changeGear(5);
+        else if(newSpeed > 150) this.changeGear(4);
+        else if(newSpeed > 100) this.changeGear(3);
+        else if(newSpeed > 50) this.changeGear(2);
+        else this.changeGear(1);
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
-        }
+        this.changeSpeed(Math.max(newSpeed, 0), this.getCurrentDirection());
     }
 }
